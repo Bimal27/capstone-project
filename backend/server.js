c01d989042052c6aeb3db2e7158cdf0caef10be0
Bimal27 <bimalsah1996@gmail.com>
2023-02-15 23:07:42 +0100

    updated

--- backend/server.js
+++ backend/server.js
@@ -1,33 +1,31 @@
-import express from 'express'
-import mongoose from 'mongoose'
-import listEndpoints from 'express-list-endpoints'
-import cookieParser from 'cookie-parser'
-import cloudinary from 'cloudinary'
-import bodyParser from 'body-parser'
-import fileUpload from 'express-fileupload'
+import express from "express";
+import mongoose from "mongoose";
+import listEndpoints from "express-list-endpoints";
+import cookieParser from "cookie-parser";
+import cloudinary from "cloudinary";
+import bodyParser from "body-parser";
+import fileUpload from "express-fileupload";
 
-import cors from 'cors'
-import productRouter from './routes/product.js'
-import errorHandler from './middleware/error.js'
+import cors from "cors";
+import productRouter from "./routes/product.js";
+import errorHandler from "./middleware/error.js";
 import {
   badRequestErrorHandler,
   notFoundErrorHandler,
   forbiddenErrorHandler,
-  genericServerErrorHandler,
-} from './errorHandlers.js'
-import path from 'path'
-import userRouter from './routes/user.js'
-import reviewRouter from './routes/review.js'
-import orderRouter from './routes/order.js'
-import paymentRouter from './routes/payment.js'
+  genericServerErrorHandler
+} from "./errorHandlers.js";
+import path from "path";
+import userRouter from "./routes/user.js";
+import reviewRouter from "./routes/review.js";
+import orderRouter from "./routes/order.js";
+import paymentRouter from "./routes/payment.js";
 
-const server = express()
+const server = express();
 
-const port = process.env.PORT || 3001
-
+const port = process.env.PORT || 3001;
 
 // const __dirname = path.resolve();
-
 
 // ************************* MIDDLEWARES ********************************
 
@@ -45,33 +43,30 @@ const corsOptions = {
   credentials: true
 };
 
-
 server.use(cors({ origin: "http://localhost:3000", credentials: true }));
 server.use(cookieParser());
 
-server.use(express.json())
-server.use(bodyParser.urlencoded({ extended: true }))
-server.use(fileUpload())
-
-
+server.use(express.json());
+server.use(bodyParser.urlencoded({ extended: true }));
+server.use(fileUpload());
 
 // ************************* ERROR MIDDLEWARES ********************************
-server.use(errorHandler)
-server.use(badRequestErrorHandler)
-server.use(notFoundErrorHandler)
-server.use(forbiddenErrorHandler)
-server.use(genericServerErrorHandler)
+server.use(errorHandler);
+server.use(badRequestErrorHandler);
+server.use(notFoundErrorHandler);
+server.use(forbiddenErrorHandler);
+server.use(genericServerErrorHandler);
 
 // ************************* Routes *******************************
-server.use('/', productRouter)
-server.use('/', reviewRouter)
+server.use("/", productRouter);
+server.use("/", reviewRouter);
 server.use("/register", userRouter);
-server.use('/', userRouter)
-server.use('/', orderRouter)
-server.use('/', paymentRouter)
+server.use("/", userRouter);
+server.use("/", orderRouter);
+server.use("/", paymentRouter);
 
-if(process.env.NODE_ENV === "production"){
-    server.use(express.static("../frontend/build"));
+if (process.env.NODE_ENV === "production") {
+  server.use(express.static("../frontend/build"));
 }
 // server.use(express.static(path.join(__dirname, "../frontend/build")));
 
@@ -80,22 +75,21 @@ if(process.env.NODE_ENV === "production"){
 // });
 
 // ************************* Mongo Connection *******************************
-mongoose.connect(process.env.MONGO_CONNECTION)
+mongoose.connect(process.env.MONGO_CONNECTION);
 
 cloudinary.config({
   cloud_name: process.env.CLOUDINARY_NAME,
   api_key: process.env.CLOUDINARY_API_KEY,
-  api_secret: process.env.CLOUDINARY_API_SECRET,
-})
+  api_secret: process.env.CLOUDINARY_API_SECRET
+});
 
-mongoose.connection.on('connected', () => {
-  console.log('Successfully connected to Mongo!')
+mongoose.connection.on("connected", () => {
+  console.log("Successfully connected to Mongo!");
   server.listen(port, () => {
-    console.table(listEndpoints(server))
-    console.log(`Server running on port ${port}`)
-  })
-})
-mongoose.connection.on('error', () => {
-  console.log(err)
-})
-
+    console.table(listEndpoints(server));
+    console.log(`Server running on port ${port}`);
+  });
+});
+mongoose.connection.on("error", () => {
+  console.log("error");
+});
