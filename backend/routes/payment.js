074c49cb99c3217506f302939bf6fd5c0d96e549
Bimal Kumar Sah <58667490+Bimal27@users.noreply.github.com>
2023-05-14 18:28:45 +0200

    Update payment.js

--- backend/routes/payment.js
+++ backend/routes/payment.js
@@ -9,7 +9,8 @@ const stripe = Stripe(process.env.STRIPE_SECRET_KEY)
 const paymentRouter = express.Router();
 
 paymentRouter.post("/payment/process", async (req, res, next) =>{
-    const myPayment = await stripe.paymentIntents.create({
+    try{
+       const myPayment = await stripe.paymentIntents.create({
     amount: req.body.amount,
     currency: "eur",
     metadata: {
@@ -24,6 +25,11 @@ paymentRouter.post("/payment/process", async (req, res, next) =>{
 
 paymentRouter.get("/stripeapikey", async (req, res, next) => {
   res.status(200).json({ stripeApiKey: process.env.STRIPE_API_KEY });
+    
+    }catch(error){
+next(error)
+}
+ 
 });
 
 export default paymentRouter
